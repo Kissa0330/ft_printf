dfbe4d82f7fb75b25ba0cc6a6b5f448df8944e24
らいか <raika4789@gmail.com>
2022-04-24 15:44:41 +0900

    split ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -1,29 +1,33 @@
 #include "ft_printf.h"
-#include <stdio.h>
+
+void	put_str(char *str, va_list args)
+{
+	size_t	len;
+
+	len = 0;
+	while(ft_strchr(str, '%') != NULL)
+	{
+		len = ft_strlen(str) - ft_strlen(ft_strchr(str, '%'));
+		while (len--)
+			ft_putchar_fd(*str++, 1);
+		if(*(str + 1) == 'd' || *(str + 1) == 'i')
+			ft_putnbr_fd(va_arg(args, int), 1);
+		else if(*(str + 1) == 's')
+			ft_putstr_fd(va_arg(args, char *), 1);
+		else if(*(str + 1) == 'c')
+			ft_putchar_fd(va_arg(args, int), 1);
+		str = str + 2;
+	}
+	ft_putstr_fd(str, 1);
+}
 
 int	ft_printf(const char *str, ...)
 {
-	size_t	len;
-	char	*str_pt;
 	va_list	args;
 
-	len = 0;
 	va_start(args, str);
-	str_pt = (char *)str;
-	while(ft_strchr(str_pt, '%') != NULL)
-	{
-		len = ft_strlen(str_pt) - ft_strlen(ft_strchr(str_pt, '%'));
-		while (len--)
-			ft_putchar_fd(*str_pt++, 1);
-		if(*(str_pt + 1) == 'd')
-			ft_putnbr_fd(va_arg(args, int), 1);
-		else if(*(str_pt + 1) == 's')
-			ft_putstr_fd(va_arg(args, char *), 1);
-		// else if(*(str_pt + 1) == 'c')
-		// 	ft_putchar_fd(va_arg(args, char), 1);
-		str_pt = str_pt + 2;
-	}
+	put_str((char *)str, args);
 	va_end(args);
-	ft_putstr_fd(str_pt, 1);
+
 	return (ft_strlen(str));
 }
--- main.c
+++ main.c
@@ -3,7 +3,14 @@ int ft_printf(const char *str, ...);
 
 int main(int argc, char const *argv[])
 {
+	int d = 3;
+	char *str = "str";
+	char c = 'a';
 	ft_printf("///////////////////////////\nft_printf test\n");
-	ft_printf("1 = %d /// 2 = %s\n", 2, "str");
+	ft_printf("nbr_test = %d /// string_test = %s /// char_test = %c\n", d, str, c);
+	ft_printf("///////////////////////////\n\n");
+	printf("///////////////////////////\nprintf test\n");
+	printf("nbr_test = %d /// string_test = %s /// char_test = %c\n", d, str, c);
+	printf("///////////////////////////\n");
 	return 0;
 }
